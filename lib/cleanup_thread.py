d01f07f292e8ecdff0191740be9d03f3829e1881
Yoram Hekma <hekma.yoram@gmail.com>
2015-01-08 10:49:12 +0100

    Revert "added locking of cleanup thread"
    This reverts commit a91b79cedb6d7bcee40f504f09dd8b47b8daad46.
    
    Conflicts:
    	lib/datamounter_helpers.py

--- lib/cleanup_thread.py
+++ lib/cleanup_thread.py
@@ -3,16 +3,13 @@ from time import sleep
 from ansible_helpers import gut_struct
 
 class cleanup_thread(threading.Thread):
-    def __init__(self, sleeptime, struct, lock):
+    def __init__(self, sleeptime, struct):
         threading.Thread.__init__(self)
         self.sleeptime = sleeptime
         self.struct = struct
         self.daemon = True
-        self.lock = lock
 
     def run(self):
         while True:
-            self.lock.acquire()
             gut_struct(self.struct)
-            self.lock.release()
             sleep(self.sleeptime)
